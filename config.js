75c7672ff606c0744e2aa2db3866afb0c7de5973
Tharakadilshan0423 <tharakadilshan0313@gmail.com>
2024-09-21 10:01:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id ",
+SESSION_ID: process.env.SESSION_ID || "CT40laBR#N5AiERkyDoRAMhBKp1RaLktwKl347XBuv4GupYv42C0",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://imgtr.ee/images/2024/09/17/37f9be869d1404ef1a5d188178450537.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm alive now",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-SUDO_NB: process.env.SUDO_NB || "94704227534",
-OWNER_NAME: process.env.OWNER_NAME || "Nethmika MAIN",
-BOT_NAME: process.env.BOT_NAME || "NETHU-MD",
+MODE: process.env.MODE || "privet",
+SUDO_NB: process.env.SUDO_NB || "94760534874",
+OWNER_NAME: process.env.OWNER_NAME || "NURO MAIN",
+BOT_NAME: process.env.BOT_NAME || "NURO-MD",
 AUTO_VOICE: process.env.AUTO_VOICE || "true"
 
 };
